751aa3a4a83bb96c9f954ea0cea704daa10499a4
HarshRohila <HarshRohila@users.noreply.github.com>
2023-08-02 03:41:53 +0000

    deploy: HarshRohila/rohilaharsh@36fc0c2b9a0084ae2d7dfcc4e2739cb548403537

--- ts/app/index.js
+++ ts/app/index.js
@@ -1,7 +1,34 @@
 (() => {
   // <stdin>
   var cdn = "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.js";
+  main();
   async function main() {
+    initializeGetStars();
+    await setupVueJs();
+  }
+  async function initializeGetStars() {
+    async function getStarsFromRepoUrl(repoUrl) {
+      try {
+        const urlParts = repoUrl.split("/");
+        const owner = urlParts[urlParts.length - 2];
+        const repo = urlParts[urlParts.length - 1];
+        const apiUrl = `https://api.github.com/repos/${owner}/${repo}`;
+        const response = await axios.get(apiUrl);
+        const { stargazers_count } = response.data;
+        return stargazers_count;
+      } catch (error) {
+        console.error("Error retrieving star count:", error);
+        return 0;
+      }
+    }
+    const APP_NAME = "web-dev-blog";
+    window[APP_NAME] = {
+      getStarsFromRepoUrl
+    };
+    const loadedEvent = new CustomEvent("script-loaded");
+    document.body.dispatchEvent(loadedEvent);
+  }
+  async function setupVueJs() {
     const Vue = await import(cdn);
     var app = Vue.createApp({
       delimiters: ["[[", "]]"],
@@ -12,5 +39,4 @@
       }
     }).mount("body");
   }
-  main();
 })();
