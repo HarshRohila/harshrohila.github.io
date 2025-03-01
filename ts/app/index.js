6045b1ca6823bbda1aa62d4c957563a860aff3e9
HarshRohila <HarshRohila@users.noreply.github.com>
2025-03-01 09:11:58 +0000

    deploy: HarshRohila/rohilaharsh@7c1335a28c20ac6f44ea3d67d325c1f04932ec8f

--- ts/app/index.js
+++ ts/app/index.js
@@ -1,41 +1,19 @@
 (() => {
   // <stdin>
-  var cdn = "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.js";
-  main();
-  async function main() {
-    initializeGetStars();
-    await setupVueJs();
+  function setScrolledPx(ev = {}) {
+    console.log(ev);
+    const scrolledPx = window.scrollY;
+    requestAnimationFrame(() => {
+      document.documentElement.style.setProperty("--scrolled-px", scrolledPx.toString());
+    });
   }
-  async function initializeGetStars() {
-    async function getStarsFromRepoUrl(repoUrl) {
-      try {
-        const urlParts = repoUrl.split("/");
-        const owner = urlParts[urlParts.length - 2];
-        const repo = urlParts[urlParts.length - 1];
-        const apiUrl = `https://api.github.com/repos/${owner}/${repo}`;
-        const response = await axios.get(apiUrl);
-        const { stargazers_count } = response.data;
-        return stargazers_count;
-      } catch (error) {
-        return 0;
-      }
-    }
-    const APP_NAME = "web-dev-blog";
-    window[APP_NAME] = {
-      getStarsFromRepoUrl
-    };
-    const loadedEvent = new CustomEvent("script-loaded");
-    document.body.dispatchEvent(loadedEvent);
+  function setHeaderHeight() {
+    const headerHeight = document.querySelector("header").clientHeight;
+    document.documentElement.style.setProperty("--header-height", headerHeight.toString());
   }
-  async function setupVueJs() {
-    const Vue = await import(cdn);
-    var app = Vue.createApp({
-      delimiters: ["[[", "]]"],
-      data() {
-        return {
-          message: "hi"
-        };
-      }
-    }).mount("body");
-  }
+  window.addEventListener("scroll", setScrolledPx);
+  window.addEventListener("resize", setScrolledPx);
+  setScrolledPx();
+  window.addEventListener("resize", setHeaderHeight);
+  setHeaderHeight();
 })();
---
+++ ts/app/projects.js
@@ -0,0 +1,41 @@
+(() => {
+  // <stdin>
+  var cdn = "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.js";
+  main();
+  async function main() {
+    initializeGetStars();
+    await setupVueJs();
+  }
+  async function initializeGetStars() {
+    async function getStarsFromRepoUrl(repoUrl) {
+      try {
+        const urlParts = repoUrl.split("/");
+        const owner = urlParts[urlParts.length - 2];
+        const repo = urlParts[urlParts.length - 1];
+        const apiUrl = `https://api.github.com/repos/${owner}/${repo}`;
+        const response = await axios.get(apiUrl);
+        const { stargazers_count } = response.data;
+        return stargazers_count;
+      } catch (error) {
+        return 0;
+      }
+    }
+    const APP_NAME = "web-dev-blog";
+    window[APP_NAME] = {
+      getStarsFromRepoUrl
+    };
+    const loadedEvent = new CustomEvent("script-loaded");
+    document.body.dispatchEvent(loadedEvent);
+  }
+  async function setupVueJs() {
+    const Vue = await import(cdn);
+    var app = Vue.createApp({
+      delimiters: ["[[", "]]"],
+      data() {
+        return {
+          message: "hi"
+        };
+      }
+    }).mount("body");
+  }
+})();
